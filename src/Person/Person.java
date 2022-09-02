9fc4394422b3663fc1d909a23c87d687607d6bd1
Ekaterina Botova <ksergeevnak@yandex.ru>
2022-09-02 13:30:01 +0300

    CollectionQueue

---
+++ src/Person/Person.java
@@ -0,0 +1,31 @@
+package Person;
+
+
+public class Person {
+    private String name;
+    private String secondName;
+    private int tickets;
+
+    public Person(String name, String secondName, int tickets) {
+        this.name = name;
+        this.secondName = secondName;
+        this.tickets = tickets;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public int getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(int tickets) {
+        this.tickets = tickets - 1;
+    }
+
+}
